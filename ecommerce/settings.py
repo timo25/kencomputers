cec1c825b53eb6a85ffa222723e52f7e20bec78e
timo25 <timothyndungo@gmail.com>
2022-03-02 19:19:33 +0300

    last commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -28,7 +28,7 @@ from decouple import config
 
 SECRET_KEY='ertwttwuuwtttwyywrrwrrwt%$#jjshhsgsg'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'kencomputers.herokuapp.com']
 
